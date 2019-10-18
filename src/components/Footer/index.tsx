933ff8cc74e7e215b9667cc9e1105768f699ad5e
sese22 <tienloinguyen22@gmail.com>
2019-10-18 11:29:32 +0700

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link } from '@app/components';
 import { withTranslation } from '@app/core';
 import { WithTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid, Link } from '@material-ui/core';
 import { config } from '@app/config';
 import { useStyles } from './styles';
 
@@ -14,11 +13,72 @@ export const BaseFooter = (props: Props): JSX.Element => {
 
   return (
     <footer className={classes.root}>
-      <Typography variant='body1'>
-        &copy; <Link href='/'>{config.author}</Link>
-        &nbsp;{config.copyRightYear}
-      </Typography>
-      <Typography variant='caption'>{t('footer.createdWithLove')}</Typography>
+      <Grid container>
+        <Grid item xs={1} lg={2}></Grid>
+        <Grid item xs={10} lg={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={3}>
+              <h3 className={classes.company}>Company</h3>
+              <div>
+                <Link color='inherit'>About us</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Contact</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Products</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Blogs</Link>
+              </div>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <h3 className={classes.company}>Legal</h3>
+              <div>
+                <Link color='inherit'>FAQ</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Premium support</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Sponsorships</Link>
+              </div>
+              <div>
+                <Link color='inherit'>Custom development</Link>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <h3 className={classes.company}>Social medias</h3>
+              <div>
+                <Link color='inherit' className={classes.socialLink}>
+                  <img src='/static/images/facebook.png' className={classes.image} />
+                </Link>
+                <Link color='inherit' className={classes.socialLink}>
+                  <img src='/static/images/dribbble.png' className={classes.image} />
+                </Link>
+                <Link color='inherit' className={classes.socialLink}>
+                  <img src='/static/images/google-plus.png' className={classes.image} />
+                </Link>
+                <Link color='inherit' className={classes.socialLink}>
+                  <img src='/static/images/linkedin.png' className={classes.image} />
+                </Link>
+                <Link color='inherit' className={classes.socialLink}>
+                  <img src='/static/images/pinterest.png' className={classes.image} />
+                </Link>
+              </div>
+            </Grid>
+          </Grid>
+
+          <div className={classes.signature}>
+            <Typography variant='body1'>
+              &copy; <Link href='/'>{config.author}</Link>
+              &nbsp;{config.copyRightYear}
+            </Typography>
+            <Typography variant='caption'>{t('footer.createdWithLove')}</Typography>
+          </div>
+        </Grid>
+        <Grid item xs={1} lg={2}></Grid>
+      </Grid>
     </footer>
   );
 };
