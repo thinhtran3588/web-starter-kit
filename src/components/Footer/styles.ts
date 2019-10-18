6501e2038699f27fb44f5eafe658adbd860d11d2
sese22 <32796559+tienloinguyen22@users.noreply.github.com>
2019-10-18 11:36:56 +0700

    Update footer (#7)

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,7 +1,23 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import { grey } from '@material-ui/core/colors';
 
 export const useStyles = makeStyles((theme) => ({
   root: {
+    backgroundColor: grey[900],
     padding: theme.spacing(2),
+    color: '#fff',
+  },
+  company: {
+    fontSize: theme.spacing(2.5),
+  },
+  socialLink: {
+    marginRight: theme.spacing(1),
+  },
+  image: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+  },
+  signature: {
+    padding: `${theme.spacing(5)}px 0px ${theme.spacing(2)}px 0px`,
   },
 }));
